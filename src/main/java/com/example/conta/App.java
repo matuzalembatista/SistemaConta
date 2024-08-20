021f0a04f0c2f0b3787dba2eb7ca6a3eb3d2a049
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-08-20 17:51:44 -0300

    Ajustes

--- src/main/java/com/example/conta/App.java
+++ src/main/java/com/example/conta/App.java
@@ -1,3 +1,34 @@
+package com.example.conta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-    
+
+	public static void main(String[] args) {
+        Agencia caixaLuizote = new Agencia("0205-08");
+        Conta contaCorrente = new ContaCorrente(caixaLuizote, "1164-9", 500d, 1000);
+        Conta contaPoupanca = new ContaPoupanca(caixaLuizote, "1324-9", 700d);
+        Conta contaSalario = new ContaSalario(caixaLuizote, "1548-7", 1500d);
+
+        caixaLuizote.addConta(contaPoupanca);
+        caixaLuizote.addConta(contaCorrente);
+        caixaLuizote.addConta(contaSalario);
+
+        try {
+            caixaLuizote.saque("1164-9", 1200);
+            caixaLuizote.saque("1324-9", 1000);
+            caixaLuizote.saque("1548-7", 500);
+
+        } catch (ContaException e) {
+            System.out.println(e.getMessage());
+        }
+
+        caixaLuizote.cobrancaDeTarifas(); // código para chamar cobrança de tarifas para contas correntes
+
+        System.out.println("Saldo Conta Corrente (" + contaCorrente.getNumeroConta() + "): " + contaCorrente.getSaldo());
+        System.out.println("Saldo Conta Poupança (" + contaPoupanca.getNumeroConta() + "): " + contaPoupanca.getSaldo());
+        System.out.println("Saldo Conta Salário (" + contaSalario.getNumeroConta() + "): " + contaSalario.getSaldo());
+    }
 }
--- src/main/java/com/example/conta/Conta.java
+++ src/main/java/com/example/conta/Conta.java
@@ -1,3 +1,48 @@
-public class Conta {
-    
+package com.example.conta;
+public abstract class Conta {
+    private Agencia agencia;
+    private String numeroConta;
+    protected double saldo;
+
+    public Conta(Agencia agencia, String numeroConta, double saldo) {
+        this.agencia = agencia;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public void deposito(double valor) throws ContaException {
+        if (valor > 0) {
+            this.saldo += valor;
+        } else {
+            throw new ContaException("Valor inválido para depósito " + valor);
+        }
+    }
+
+    public void saque(double valor) throws ContaException {
+        if (valor > 0) {
+            if (this.saldo >= valor) {
+                this.saldo -= valor;
+            } else {
+                throw new ContaException(this.getNumeroConta(), "sem saldo suficiente: " + this.saldo);
+            }
+        } else {
+            throw new ContaException(this.getNumeroConta(), "Valor inválido para saque: " + valor);
+        }
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void cobrancaDeTarifa() {
+      
+    }
 }
